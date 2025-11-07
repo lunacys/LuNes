0070e7ee828e3b7b086a02a46d6bc36f828498a9
Grigory Alfyorov <loonacuse@gmail.com>
2025-11-07 14:20:33 +0500

    Initial commit

---
+++ LuNes.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using ImGuiNET;
+using Raylib_cs;
+using rlImGui_cs;
+
+namespace LuNes.Client;
+
+static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        Raylib.InitWindow(800, 480, "Hello World!");
+        rlImGui.Setup();
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            
+            rlImGui.Begin();
+            ImGui.Begin("Hello");
+            ImGui.Text("World");
+            ImGui.End();
+            rlImGui.End();
+            
+            Raylib.EndDrawing();
+        }
+        
+        rlImGui.Shutdown();
+        Raylib.CloseWindow();
+    }
+}
